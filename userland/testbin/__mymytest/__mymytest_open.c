23019e2992a7377e1928176e7ab511ecbb9ed73c
Andrew Wong <andrew.j.wong@outlook.com>
2020-03-30 20:30:24 +1100

    Backup :: Mon 30 Mar 20:30:24 AEDT 2020

--- userland/testbin/__mymytest/__mymytest_open.c
+++ userland/testbin/__mymytest/__mymytest_open.c
@@ -1,14 +1,18 @@
 #include <stdlib.h>
 #include <err.h>
 #include <errno.h>
+ #include <sys/types.h>
 
 #include "__mymytest.h"
 
-#define TEST_FILENAME "insert_testing_filename.txt"
+#define TEST_VALID_FILENAME "test.file"
+#define TEST_INVALID_FILENAME "test.file"
+#define TEST_MODE 0700
 
 static void test_open__noFlagsProvided(void); // EINVAL
 static void test_open__invalidFilename(void); // EFAULT
 static void test_open__filetable_full(void); // EMFILE
+
 
 void test_open() {
     test(test_open__noFlagsProvided);
@@ -18,13 +22,14 @@ void test_open() {
 
 static void test_open__noFlagsProvided() { 
     int fd; 
-    
-    // fd = fopen(TEST_FILENAME, ); 
+    _asssert((fd = open(TEST_FILENAME, NULL, TEST_MODE)) == -1); 
+    // _assert(errno == ) 
+    // TODO: Not sure what it should be equal to .. VFS man page doesn't tell us the return error values 
     
     return;
 }
 static void test_open__invalidFilename() {
-    
+    _asssert((fd = open(TEST_FILENAME, NULL, TEST_MODE)) == -1); 
     return;
 }
 static void test_open__filetable_full() {

3bb6bb11ccde9bdbe0ce37dc79f03d54358d47c4
Andrew Wong <andrew.j.wong@outlook.com>
2020-03-30 20:40:24 +1100

    Backup :: Mon 30 Mar 20:40:24 AEDT 2020

--- userland/testbin/__mymytest/__mymytest_open.c
+++ userland/testbin/__mymytest/__mymytest_open.c
@@ -1,12 +1,13 @@
 #include <stdlib.h>
 #include <err.h>
 #include <errno.h>
- #include <sys/types.h>
+#include <sys/types.h>
+#include <string.h>
 
 #include "__mymytest.h"
 
 #define TEST_VALID_FILENAME "test.file"
-#define TEST_INVALID_FILENAME "test.file"
+#define TEST_INVALID_FILENAME "random.file"
 #define TEST_MODE 0700
 
 static void test_open__noFlagsProvided(void); // EINVAL
@@ -22,16 +23,31 @@ void test_open() {
 
 static void test_open__noFlagsProvided() { 
     int fd; 
-    _asssert((fd = open(TEST_FILENAME, NULL, TEST_MODE)) == -1); 
+
+    // Only doing this because I don't know where to keep the file
+    fd = open(TEST_VALID_FILENAME, O_RDWR | O_CREAT, TEST_MODE); 
+    close(fd); 
+
+    _asssert((fd = open(TEST_VALID_FILENAME, NULL, TEST_MODE)) == -1); 
     // _assert(errno == ) 
     // TODO: Not sure what it should be equal to .. VFS man page doesn't tell us the return error values 
     
     return;
 }
 static void test_open__invalidFilename() {
-    _asssert((fd = open(TEST_FILENAME, NULL, TEST_MODE)) == -1); 
+
+    _asssert((fd = open(TEST_FILENAME, O_RDWR , TEST_MODE)) == -1); 
+    _assert(errno == EFAULT); 
     return;
 }
 static void test_open__filetable_full() {
+
+    int fd;
+    char *file_name; 
+    for (int i = 3; i < 128; i++) { 
+        file_name = strcat(TEST_VALID_FILENAME, str(i)); // TODO: Test if this works
+        
+        fd = open()
+    }
     return;
 }
